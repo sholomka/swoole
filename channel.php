3b1584db3a7b4dc2f8b39cded2255c065a35faf7
sholomka <sholomka@gmail.com>
2021-04-10 13:38:57 +0300

    channel

--- channel.php
+++ channel.php
@@ -1,1 +1,24 @@
 <?php
+
+$chan = new Swoole\Coroutine\Channel(1);
+
+Co\Run(function() use ($chan) {
+    $cid = Swoole\Coroutine::getuid();
+    $i = 0;
+
+    while(1) {
+        co::sleep(1.0);
+        $chan->push(['rand' => rand(1000, 9999), 'index' => $i]);
+        echo "[coroutine $cid] - $i\n";
+        $i++;
+    }
+});
+
+Co\Run(function() use ($chan) {
+    $cid = Swoole\Coroutine::getuid();
+    while(1) {
+        $data = $chan->pop();
+        echo "[coroutine $cid]\n";
+        var_dump($data);
+    }
+});
